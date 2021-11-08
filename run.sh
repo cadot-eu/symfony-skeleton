bb34fbff8f3388942ed39599417595ec3c4f0164
michael <michael@cadot.info>
2021-11-08 11:33:13 +0000

    add port

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
-export result=${PWD##*/}
+result=${PWD##*/}
 docker-compose down --remove-orphans
+sed -i 's?{DIR}?'$result'?' docker-compose.yml
 docker-compose up -d
+docker inspect --format='{{range $p, $conf := .NetworkSettings.Ports}}  {{(index $conf 0).HostPort}} {{end}}' $result
